4ee84494842714a3edc43f55823613f7f17e7efe
ride-a-wave <15720606503@163.com>
2020-10-26 00:33:18 +0800

    GitHubpush,pull测试

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("11111111111222222");
         System.out.println("11111111111333333");
+        System.out.println("github  push ,pull测试");
     }
 }
